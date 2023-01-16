d7b4773f2331d68e986b592659c35b3e222ff3b7
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2023-01-16 20:56:28 +0200

    Befor refactoring (task 4)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import {ContactForm} from "./ContactForm/ContactForm";
+
+
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  visibleContacts = () => {
+    const arr = this.state.contacts.filter(contact => contact.name.toLowerCase().includes(this.state.filter.toLowerCase()));
+    console.log(arr);
+    return arr;
+  };  
+
+  handleSubmit = evt => {
+    let contactsState = this.state.contacts;
+    contactsState.push({ id: nanoid(), name: evt.name, number: evt.number });
+    this.setState({ contacts: contactsState });
+  };
+
+  findContacts = evt => {
+    const { value } = evt.target;
+    this.setState({ filter: value });
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <div>
+          <h2>Phonebook</h2>
+          <ContactForm onSubmit={this.handleSubmit}></ContactForm>                 
+          <h2>Contacts</h2>
+          <p>Find contacts by name</p>
+          <input type="text" onChange={this.findContacts} />
+          <ul>
+            {this.visibleContacts().map(({ id, name, number }) => (
+              <li key={id}>
+                {name}: {number}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,20 @@
+import { Formik, Form, Field } from 'formik';
+
+export const ContactForm = ({ onSubmit }) => {
+  const initialValues = {
+    name: '',
+    number: '',
+  };
+    
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      <Form>
+        <p>Name: </p>
+        <Field type="text" name="name" required></Field>
+        <p>Number: </p>
+        <Field type="tel" name="number" required></Field>
+        <button type="submit">Add contact</button>
+      </Form>
+    </Formik>
+  );
+};
